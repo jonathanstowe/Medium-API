5b2fcc892cb1c6d19ba6d26d83115585f7e457ef
Jonathan Stowe <jns@gellyfish.co.uk>
2016-08-29 09:19:27 +0100

    tougher constraints in tag, title

--- lib/Medium/API.pm
+++ lib/Medium/API.pm
@@ -12,11 +12,13 @@ class Medium::API {
     class Post does JSON::Class {
         subset PublishStatus of Str where "public"|"draft"|"unlisted";
         has PublishStatus $.publish-status is json-name('publishStatus') = "public";
-        has Str $.title is required;
+        subset Title of Str where .chars <= 100 ;
+        has Title $.title is required;
         subset ContentFormat of Str where "html"|"markdown";
         has ContentFormat $.content-format is json-name('contentFormat') is required;
         has Str $.content is required;
-        has     @.tags;
+        subset Tag of Str where .chars < 26;
+        has Tag @.tags;
         has Str $.canonical-url  is json-name('canonicalUrl') is json-skip-null;
         subset Licence of Str where "all-rights-reserved"|"cc-40-by"|"cc-40-by-sa"|"cc-40-by-nd"|"cc-40-by-nc"|"cc-40-by-nc-nd"|"cc-40-by-nc-sa"|"cc-40-zero"|"public-domain";
         has Licence $.license = "all-rights-reserved";
